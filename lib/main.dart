9be9bbacc2b48c7c6d42587b384cc8c8431b080f
ThanksKiet <0306211159@caothang.edu.vn>
2023-12-04 14:54:12 +0700

    l

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:smarthome/firebase_options.dart';
-<<<<<<< HEAD
 import 'package:smarthome/views/AccountManage.dart';
-=======
-import 'package:smarthome/views/DK_TaiKhoan.dart';
->>>>>>> a0ee346095469ac0e6dd32305cfc74f2ebda4a70
-import 'package:smarthome/views/DetailRoomScreen.dart';
-import 'package:smarthome/views/HomeScreen.dart';
-import 'package:smarthome/views/LoginScreen.dart';
-import 'package:smarthome/views/ProfileScreen.dart';
+import './views/LoginScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,15 +21,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
         useMaterial3: true,
       ),
-<<<<<<< HEAD
-      home: QL_TaiKhoan(),
-=======
       home: LoginPage(),
->>>>>>> a0ee346095469ac0e6dd32305cfc74f2ebda4a70
       // routes: {
       //   "/home": (context) => HomeScreen(),
       //   "/profile": (context) => ProfileScreen(),
       // },
+      // initialRoute: '/',
     );
   }
 }
--- lib/views/DK_TaiKhoan.dart
+++ lib/views/DK_TaiKhoan.dart
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
->>>>>>> origin/main
+import 'LoginScreen.dart';
 
 class DkTaiKhoan extends StatefulWidget {
   @override
